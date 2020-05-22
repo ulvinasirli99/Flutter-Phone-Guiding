b6ceaf036a39483d710485fe976b3df16cc58c4f
ulvi1 <ulvi.1999rufet@gmail.com>
2020-05-22 16:25:58 +0400

    Flutter Phone Guiding

---
+++ android/app/src/main/kotlin/neo/devloper/flutterapp05182020/MainActivity.kt
@@ -0,0 +1,12 @@
+package neo.devloper.flutterapp05182020
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/basic/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp05182020/model/contact_page.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: ContactPage());
+  }
+}
